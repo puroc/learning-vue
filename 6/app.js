55ab6c0ed8a1dad93a1545f30656940e508a4f75
puroc <p00u00r00>
2018-02-20 15:03:53 +0800

    1

---
+++ 6/app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        age: 37,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        add: function (num) {
+            this.age += num;
+        },
+        subtract: function (num) {
+            this.age -= num;
+        },
+        updatexy: function (event) {
+            console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        stopMoving:function(event){
+            event.stopPropagation();
+        }
+
+    }
+})
