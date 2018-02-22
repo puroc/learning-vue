62e288013f2bce491bf89eb2bbe83d92ac84b7c5
puroc <p00u00r00>
2018-02-22 12:11:24 +0800

    1

--- user-management/src/App.vue
+++ user-management/src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="app">
+  <div id="app ">
+      <nav class="navbar navbar-default container">
+        <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            <a class="navbar-brand" href="#"><img src="./assets/logo.png" alt="" width="20" height="20"></a>
+          </div>
   
-   <nav class="navbar navbar-default">
-      <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">用户管理系统</a>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+            <ul class="nav navbar-nav">
+              <li>
+                <router-link to="/">主页</router-link>
+              </li>
+              <li>
+                <router-link to="/about">关于</router-link>
+              </li>
+            </ul>
+          </div>
+          <!-- /.navbar-collapse -->
         </div>
+      </nav>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
-          <ul class="nav navbar-nav">
-            <li><router-link to="/">主页</router-link></li>
-            <li><router-link to="/about">关于</router-link></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div>
-    </nav>
     <router-view/>
   </div>
 </template>
--- user-management/src/components/Customers.vue
+++ user-management/src/components/Customers.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="customers">
-    customers
+  <div class="customers container">
+    <h1 class="page-header">用户管理系统</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>电话</th>
+          <th>邮箱</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers">
+          <td>{{customer.name}}</td>
+          <td>{{customer.age}}</td>
+          <td>{{customer.phone}}</td>
+          <td>{{customer.email}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,9 +27,20 @@ export default {
   name: 'customers',
   data () {
     return {
-      
+      customers:[]
     }
+  },
+  methods:{
+    fetchCustomers(){
+      this.$http.get("http://localhost:3000/users").then(function(response){
+        this.customers =  response.body;
+      })
+    }
+  },
+  created(){
+    this.fetchCustomers();
   }
+  
 }
 </script>
 
